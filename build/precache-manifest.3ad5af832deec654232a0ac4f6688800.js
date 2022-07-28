6a6d9e17a3d714e11338ed56f864524b7fb41de5
johnnyloreano <laureano.pedrojoao@gmail.com>
2022-07-28 16:43:15 -0300

    new build

---
+++ build/precache-manifest.3ad5af832deec654232a0ac4f6688800.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bdd3c6de6e19c3d86a1cc90fa852c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ec378dc4bd79f23de3",
+    "url": "/static/css/main.df8f2434.chunk.css"
+  },
+  {
+    "revision": "410bedd25cb01fbaaeec",
+    "url": "/static/js/2.3b08e1ab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3b08e1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ec378dc4bd79f23de3",
+    "url": "/static/js/main.da401d32.chunk.js"
+  },
+  {
+    "revision": "28b0532df3468a656916",
+    "url": "/static/js/runtime-main.df8c8be7.js"
+  },
+  {
+    "revision": "00cb1559af0fc1a53f18cd010a22abcc",
+    "url": "/static/media/idear2.00cb1559.png"
+  },
+  {
+    "revision": "b16ada0cc6f797b174d283cb06c6171f",
+    "url": "/static/media/interview.b16ada0c.png"
+  },
+  {
+    "revision": "d0b4a4b6cda69fa5a76375a0340311da",
+    "url": "/static/media/invisible-button.d0b4a4b6.png"
+  },
+  {
+    "revision": "4046aaaa1b4f14bb35b370ebfb36d900",
+    "url": "/static/media/logo.4046aaaa.png"
+  },
+  {
+    "revision": "ea55dbdf758458739ff0b620b30be8f3",
+    "url": "/static/media/logo2.ea55dbdf.png"
+  },
+  {
+    "revision": "83757f274e3dad1a9449c47c4636fd9d",
+    "url": "/static/media/teacher.83757f27.png"
+  }
+]);
