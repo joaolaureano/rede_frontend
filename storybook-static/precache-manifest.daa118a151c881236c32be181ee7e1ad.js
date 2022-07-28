b5d0e89ece815d30b556324103f964ec7f302759
johnnyloreano <laureano.pedrojoao@gmail.com>
2022-07-28 15:22:34 -0300

    sTORYBOOK

---
+++ storybook-static/precache-manifest.daa118a151c881236c32be181ee7e1ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aff0809a765d6de1204b2be52d5bce",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ec5279bced99abbff8b0.bundle.js"
+  },
+  {
+    "url": "runtime~main.ec5279bced99abbff8b0.bundle.js"
+  },
+  {
+    "revision": "5c63758e008d196d05a6",
+    "url": "static/css/main.67f09c61.chunk.css"
+  },
+  {
+    "revision": "d0b4a4b6cda69fa5a76375a0340311da",
+    "url": "static/media/invisible-button.d0b4a4b6.png"
+  },
+  {
+    "revision": "ea55dbdf758458739ff0b620b30be8f3",
+    "url": "static/media/logo2.ea55dbdf.png"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "revision": "83757f274e3dad1a9449c47c4636fd9d",
+    "url": "static/media/teacher.83757f27.png"
+  },
+  {
+    "url": "vendors~main.ec5279bced99abbff8b0.bundle.js"
+  },
+  {
+    "url": "vendors~main.ec5279bced99abbff8b0.bundle.js.LICENSE.txt"
+  }
+]);
