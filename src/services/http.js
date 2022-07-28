ab7a7bfe0477a16f0134cdf7f8c90288085cedec
johnnyloreano <laureano.pedrojoao@gmail.com>
2022-07-28 16:38:48 -0300

    Fix port

--- src/services/http.js
+++ src/services/http.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 // const api = axios.create({baseURL: 'localhost:3000'})
 
 const apiServer = `${(process.env.REACT_APP_API_URL) ? process.env.REACT_APP_API_URL : 'localhost'}`;
-const port = (process.env.REACT_APP_API_PORT) ? process.env.REACT_APP_API_PORT : '3000';
 
-console.log(`BASE_URL ${apiServer}:${port}`);
+
+console.log(`BASE_URL ${apiServer}`);
 
 const client = axios.create({
   baseURL: `${apiServer}`,
